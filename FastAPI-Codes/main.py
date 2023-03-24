c121032835620a97c6041f7227b29ac61290b289
Straub Corinne BFS <Corinne.Straub@bfs.admin.ch>
2023-03-24 09:25:11 +0100

    refactoring main

--- FastAPI-Codes/main.py
+++ FastAPI-Codes/main.py
@@ -15,33 +15,33 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
  
 app = FastAPI()
 
-@app.get("/fuellstandGasspeicher")
-# async def index():
-#     return {"message": "Hallo Welt!"}
-def get_ogd102():   
+def load_csv(url):
     proxies = {
           "http":'http://proxy-bvcol.admin.ch:8080',
           "https":'http://proxy-bvcol.admin.ch:8080'
         }
-    url = 'https://bfe-energy-dashboard-ogd.s3.amazonaws.com/ogd102_fuellstand_gasspeicher.csv'
     s = requests.get(url, proxies = proxies, verify = False).text
     df = pd.read_csv(io.StringIO(s))
     df2=df.head(5)
     df1=df2.T
-    return {'data': df1}, 200  # return data and 200 OK code
+    return df1
+
+
+
+@app.get("/fuellstandGasspeicher")
+# async def index():
+#     return {"message": "Hallo Welt!"}
+def get_ogd102():   
+    url = 'https://bfe-energy-dashboard-ogd.s3.amazonaws.com/ogd102_fuellstand_gasspeicher.csv'
+    df = load_csv(url)
+    return {'datasource': url}, {'data': df}, 200  # return data and 200 OK code
+
 
 
 @app.get("/stromverbrauch")
 # async def index():
 #     return {"message": "Hallo Welt!"}
 def get_ogd103():   
-    proxies = {
-          "http":'http://proxy-bvcol.admin.ch:8080',
-          "https":'http://proxy-bvcol.admin.ch:8080'
-        }
     url = 'https://bfe-energy-dashboard-ogd.s3.amazonaws.com/ogd103_stromverbrauch_geschaetzt_swissgrid.csv'
-    s = requests.get(url, proxies = proxies, verify = False).text
-    df = pd.read_csv(io.StringIO(s))
-    df2=df.head(5)
-    df1=df2.T   
-    return {'datasource': url}, {'data': df1}, 200  # return data and 200 OK code
+    df = load_csv(url)
+    return {'datasource': url}, {'data': df}, 200  # return data and 200 OK code
